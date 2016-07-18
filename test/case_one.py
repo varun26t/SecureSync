015b12b97202418e1679c0e11f6fe1c6d195cea5
nerditall <vtewari26@gmail.com>
2016-07-18 16:27:12 +0530

    Edit test case and travis.yml

--- test/case_one.py
+++ test/case_one.py
@@ -1,18 +1,20 @@
 from base64 import b64encode, b64decode
 
-FILE = '../requirements.txt'
-TEMP_FILE = '../data/TEMPORARY_FILE'
+#FILE = '../requirements.txt'
+#TEMP_FILE = '../data/TEMPORARY_FILE'
 
-with open(FILE, 'rb+') as file:
-	with open(TEMP_FILE, 'wb+') as out_file:
-		file_bytes = b64encode(file.read())
+#with open(FILE, 'rb+') as file:
+#	with open(TEMP_FILE, 'wb+') as out_file:
+#		file_bytes = b64encode(file.read())
 		# print(file_bytes)
-		out_file.write(file_bytes)
-		print("Success")
+#		out_file.write(file_bytes)
+#		print("Success")
 
 
-with open(TEMP_FILE, 'rb+') as damn:
-	with open('../data/temp', 'wb+') as dam:
-		bt = b64decode(damn.read())
-		# print(bt)
-		dam.write(bt)
+#with open(TEMP_FILE, 'rb+') as damn:
+#	with open('../data/temp', 'wb+') as dam:
+#		bt = b64decode(damn.read())
+#		# print(bt)
+#		dam.write(bt)
+
+print("success")
